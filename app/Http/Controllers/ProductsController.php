2050e2edda2b27119754d16f0b25992f79cf5a5d
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-26 17:57:38 -0300

    Finalizando o CRUD

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,4 +14,20 @@ class ProductsController extends Controller
     public function store(Request $requestEloquent){
         return Product::create($requestEloquent->all());
     }
+
+    public function update(Request $requestEloquent, Product $product){
+        $product->update($requestEloquent->all());
+
+        return $product;
+    }
+
+    public function show(Product $product){
+        return $product;
+    }
+
+    public function destroy(Request $requestEloquent, Product $product){
+        $product->delete();
+
+        return $product;
+    }
 }
