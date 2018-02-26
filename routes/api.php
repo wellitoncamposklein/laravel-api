5476dcc52147416022738c76415e1e6fd4bad776
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-26 18:21:01 -0300

    Trabalhando com mais de um resource

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->group(function (){
+    Route::resources([
+        'products'=>'ProductsController',
+        'users'=>'UsersController'
+    ]);
+    /*Route::resource('products','ProductsController');
+    OR
     Route::get('/products','ProductsController@index');
     Route::post('/products','ProductsController@store');
     Route::put('/products/{product}','ProductsController@update');
     Route::get('/products/{product}','ProductsController@show');
-    Route::delete('/products/{product}','ProductsController@destroy');
+    Route::delete('/products/{product}','ProductsController@destroy');*/
 });
