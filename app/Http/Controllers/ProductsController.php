15395fea4744082f71e9a40db47c981e9efd5d63
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-26 17:25:28 -0300

    Nosso primeiro end-point

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+
+class ProductsController extends Controller
+{
+    public function index(){
+        return Product::all();
+    }
+}
