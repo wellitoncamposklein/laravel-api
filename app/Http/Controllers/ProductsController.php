a1b9b03d351132ac917d30e6773f4dfc786fb816
Welliton Klein <welliton.fokushima@gmail.com>
2018-02-27 11:01:17 -0300

    Cache dos resultados

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductsRequest as Request;
 use App\Product;
+use Carbon\Carbon;
 
 class ProductsController extends Controller
 {
     public function index(){
-        return Product::all();
+        $minutes = \Carbon\Carbon::now()->addMinutes(10);
+        //Trabalhando com CACHE a baixo
+        $products = \Cache::remember('api::products',$minutes, function (){
+            return Product::all();
+        });
+
+        return $products;
     }
 
     public function store(Request $requestEloquent){
+        //apagando o CACHE a baico
+        \Cache::forget('api::products');
+
         $data = $requestEloquent->all();
         $data['user_id'] = \Auth::user()->id;
         return Product::create($data);
